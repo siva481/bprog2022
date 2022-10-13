ee51fe4a986fe0cf27f96dff9ce9e7cb3b87cb88
*shibauchi <sibanao481@gmail.com>
2022-10-13 16:16:08 +0900

    10/13

--- src/Rec.hs
+++ src/Rec.hs
@@ -21,3 +21,32 @@ fac 0 = 1
 fac n 
     | n > 0    = n * fac (n - 1)
     |otherwise = 0
+
+ -- 
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs 
+merge (x: xs) (y:ys)
+   | x > y = y : merge  (x:xs) ys
+   |otherwise = x : merge xs (y: ys)
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = case halve xs of
+    (ys, zs) -> merge (msort ys) (msort zs)
+
+halve :: [a] -> ([a], [a])
+halve xs = (take h xs, drop h xs)
+    where
+        h = length xs `div` 2
+
+sample :: [Int]
+sample = [3,1,4,1,5,9,2,6,5,3,5,8,9]
+
+twice :: (a -> a ) -> (a -> a )
+twice f x = f (f x ) 
+
+inc :: Int-> Int 
+inc x = x + 1
+
